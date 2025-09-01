f32bb6abb09b8901fe9f5472ee4d3acac07146e8
Ramon Pratar <ramon.prata@dtidigital.com.br>
2025-08-31 21:33:10 -0300

    fix layout bugs

--- src/features/Episodes/views/EpisodesScreen.tsx
+++ src/features/Episodes/views/EpisodesScreen.tsx
@@ -1,9 +1,15 @@
 import Header from "@/src/_app/views/Header";
+import * as ScreenOrientation from "expo-screen-orientation";
 
+import { useFocusEffect } from "@/.expo/types/router";
 import { Container } from "./styles/EpisodesScreen.styled";
 import Videos from "./Videos";
 
 const EpisodesScreen = () => {
+  useFocusEffect(() => {
+    ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT_UP);
+  });
+
   return (
     <Container>
       <Header />
